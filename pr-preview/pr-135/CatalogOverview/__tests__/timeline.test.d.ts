d4a8942f52c09ca49b054f83a9da9aa468ce1534
IanMayo <IanMayo@users.noreply.github.com>
2026-01-30 14:30:19 +0000

    deploy: eda0fb0e17b6797e44e4cd2f7b78dec5be015522

---
+++ pr-preview/pr-135/CatalogOverview/__tests__/timeline.test.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Unit tests for timeline layout logic in CatalogOverview.
+ *
+ * Tests the time axis scale computation and bar positioning helpers
+ * that are used internally by the component.
+ */
+export {};
+//# sourceMappingURL=timeline.test.d.ts.map
---
+++ pr-preview/pr-135/FeatureList/FeatureRow.d.ts
@@ -0,0 +1,18 @@
+import { CSSProperties } from '../../../../node_modules/.pnpm/react@18.3.1/node_modules/react';
+import { DebriefFeature } from '../utils/types';
+
+export interface FeatureRowProps {
+    /** The feature to display */
+    feature: DebriefFeature;
+    /** Whether this row is selected */
+    isSelected: boolean;
+    /** Click handler */
+    onClick: () => void;
+    /** Optional inline style */
+    style?: CSSProperties;
+}
+/**
+ * FeatureRow displays a single feature in the list.
+ */
+export declare function FeatureRow({ feature, isSelected, onClick, style, }: FeatureRowProps): import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=FeatureRow.d.ts.map
