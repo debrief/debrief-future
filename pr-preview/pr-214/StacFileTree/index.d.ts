388eb120a0186fbd2c5f066b35917477dad961d0
IanMayo <IanMayo@users.noreply.github.com>
2026-02-10 19:22:46 +0000

    deploy: 20aecc46ed30ab7cac9dd0c2e7b9c354dac39705

--- pr-preview/pr-214/StacFileTree/index.d.ts
+++ pr-preview/pr-214/StacFileTree/index.d.ts
@@ -4,5 +4,5 @@
 export { StacFileTree } from './StacFileTree';
 export type { StacFileTreeProps, FilesystemAdapter, DirectoryEntry, FileStat, TreeNodeData, NodeType, } from './types';
 export { computeHighlightSets } from './highlightUtils';
-export { createPopulatedStore, createEmptyStore, createSingleItemStore, createStoreWithSnapshots, createMemfsAdapter, } from './fixtures';
+export { createMapAdapter } from './storyFixtures';
 //# sourceMappingURL=index.d.ts.map
