39f1ce5cc571faac6a0ab4eff57edfd1416af4d3
IanMayo <IanMayo@users.noreply.github.com>
2026-02-13 16:47:50 +0000

    deploy: bed0fe42903d278c8ca8089d81c638640b34cd45

--- pr-preview/pr-232/MapView/index.d.ts
+++ pr-preview/pr-232/MapView/index.d.ts
@@ -2,4 +2,6 @@ export { MapView } from './MapView';
 export type { MapViewProps } from './MapView';
 export { LeafletToolbar } from './LeafletToolbar';
 export type { LeafletToolbarProps } from './LeafletToolbar';
+export { useGeoman } from './GeomanControl';
+export type { UseGeomanOptions, UseGeomanReturn } from './GeomanControl';
 //# sourceMappingURL=index.d.ts.map
