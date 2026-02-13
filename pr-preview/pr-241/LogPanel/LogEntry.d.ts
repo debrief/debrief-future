0b404cdb91a70e56d7ce928b3adb7396648d614a
IanMayo <IanMayo@users.noreply.github.com>
2026-02-13 16:20:29 +0000

    deploy: a2daa29d5f0fdc21b13c1b06d30bba9795782fa5

---
+++ pr-preview/pr-241/LogPanel/LogEntry.d.ts
@@ -0,0 +1,5 @@
+import { default as React } from '../../../../node_modules/.pnpm/react@18.3.1/node_modules/react';
+import { LogEntryProps } from './types';
+
+export declare function LogEntry({ entry, featureNames, presentationMode, isSelected, onClick, onTuneClick, onRestoreClick, className, }: LogEntryProps): React.ReactElement;
+//# sourceMappingURL=LogEntry.d.ts.map
---
+++ pr-preview/pr-241/MapView/GeomanControl/useGeoman.test.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=useGeoman.test.d.ts.map
