e884d435035d901fbf381623bd0e2bf5297ad098
IanMayo <IanMayo@users.noreply.github.com>
2026-02-14 15:27:55 +0000

    deploy: bc30e6426e225bc43f7d4543aab886d2587df4fd

---
+++ pr-preview/pr-256/MapView/drawing/index.d.ts
@@ -0,0 +1,5 @@
+export { DEFAULT_DRAWN_POINT_STYLE, DEFAULT_DRAWN_RECTANGLE_STYLE, DEFAULT_DRAWN_POLYGON_STYLE, DEFAULT_DRAWN_POLYLINE_STYLE, } from './drawingDefaults';
+export { isValidDrawnGeometry } from './isValidDrawnGeometry';
+export { createDrawnFeature } from './createDrawnFeature';
+export type { CreateDrawnFeatureOptions } from './createDrawnFeature';
+//# sourceMappingURL=index.d.ts.map
