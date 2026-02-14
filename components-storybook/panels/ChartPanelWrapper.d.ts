7fe07b2130ab083ac97ddd011b1dd331364a34ef
IanMayo <IanMayo@users.noreply.github.com>
2026-02-14 22:17:10 +0000

    deploy: 099d4cbc06d602563f7845968a90236b897c8209

---
+++ components-storybook/panels/ChartPanelWrapper.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Chart Panel wrapper — renders ChartRenderer in a GoldenLayout panel.
+ */
+export declare function ChartPanelWrapper(): import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=ChartPanelWrapper.d.ts.map
