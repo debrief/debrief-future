49506ebfe6bc74b49bc89b069a8b3b39c509b518
IanMayo <IanMayo@users.noreply.github.com>
2026-03-07 16:04:48 +0000

    deploy: f1eb343f986f2cc82010b40120aa52230183d6e1

---
+++ pr-preview/pr-322/LogPanel/LogPanel.d.ts
@@ -0,0 +1,5 @@
+import { default as React } from '../../../../node_modules/.pnpm/react@18.3.1/node_modules/react';
+import { LogPanelProps } from './types';
+
+export declare function LogPanel({ entries, featureNames, presentationMode, viewMode, selectedEntryId, filterState, hasActiveSession, actionResultMessage, replayProgress, onMessage, onPresentationModeChange, onViewModeChange, onFilterStateChange, onSelectedEntryChange, onTuneRequest, onRestoreRequest, onReplayCancel, onSchemaRequest, onDisableToggle, onRationaleUpdate, className, }: LogPanelProps): React.ReactElement;
+//# sourceMappingURL=LogPanel.d.ts.map
