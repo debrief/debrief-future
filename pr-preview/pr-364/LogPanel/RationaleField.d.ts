964785f510f4e30a371cd97ca0ad3fdd9b95b090
IanMayo <IanMayo@users.noreply.github.com>
2026-03-27 14:37:50 +0000

    deploy: de6000f76f392e3c9ccb9bd6cdd27e3cd796413d

---
+++ pr-preview/pr-364/LogPanel/RationaleField.d.ts
@@ -0,0 +1,9 @@
+import { default as React } from '../../../../node_modules/.pnpm/react@18.3.1/node_modules/react';
+
+export interface RationaleFieldProps {
+    readonly value: string;
+    readonly onChange: (text: string) => void;
+    readonly inputRef?: React.Ref<HTMLTextAreaElement>;
+}
+export declare function RationaleField({ value, onChange, inputRef, }: RationaleFieldProps): React.ReactElement;
+//# sourceMappingURL=RationaleField.d.ts.map
