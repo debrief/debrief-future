a7637df8adc58cd7edcd642bf3396ecfd9961bda
IanMayo <IanMayo@users.noreply.github.com>
2026-04-10 13:43:05 +0000

    deploy: 3a8d21d51d37a03fd17393adc0e81343096d5aef

---
+++ pr-preview/pr-408/ExerciseListView/constants.d.ts
@@ -0,0 +1,5 @@
+import { ThumbnailSize, ThumbnailSizeConfig } from './types';
+
+/** Thumbnail size configurations keyed by preset. */
+export declare const THUMBNAIL_SIZE_CONFIGS: Record<ThumbnailSize, ThumbnailSizeConfig>;
+//# sourceMappingURL=constants.d.ts.map
