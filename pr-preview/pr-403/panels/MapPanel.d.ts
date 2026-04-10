571c939b39f50dcfe69173c4edaa1520962e45af
IanMayo <IanMayo@users.noreply.github.com>
2026-04-10 10:49:37 +0000

    deploy: f2b52096ae524bd8f5f4b8b03f89fdc575b7f3e9

---
+++ pr-preview/pr-403/panels/MapPanel.d.ts
@@ -0,0 +1,4 @@
+import { PanelProps } from '../PanelWorkspace/panelRegistry';
+
+export declare function MapPanel(props: PanelProps): import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=MapPanel.d.ts.map
