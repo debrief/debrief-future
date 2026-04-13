7dcc4c7fc35b923a531ec69b2f5398cc62b56db8
IanMayo <IanMayo@users.noreply.github.com>
2026-04-13 22:17:54 +0000

    deploy: 6495656dff5f5d879aa51a59ea35762e01df2928

---
+++ pr-preview/pr-425/StacBrowser/StacBrowser.d.ts
@@ -0,0 +1,5 @@
+import { default as React } from '../../../../node_modules/.pnpm/react@18.3.1/node_modules/react';
+import { StacBrowserProps } from './types';
+
+export declare const StacBrowser: React.FC<StacBrowserProps>;
+//# sourceMappingURL=StacBrowser.d.ts.map
