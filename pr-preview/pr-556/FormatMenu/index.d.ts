b8a0e75ab3d8ba2b3d737160bbe62fe70131d8dd
IanMayo <IanMayo@users.noreply.github.com>
2026-04-26 14:24:33 +0000

    deploy: 9730e808fd19a8cee1cbee1dd5b15855dc8c6716

---
+++ pr-preview/pr-556/FormatMenu/index.d.ts
@@ -0,0 +1,8 @@
+export { FormatMenu } from './FormatMenu';
+export type { FormatMenuProps } from './FormatMenu';
+export { buildFormatMenuItems, parseMenuItemId, resolvePresetValue } from './formatMenuItems';
+export type { StylePropertyDescriptor } from './formatMenuItems';
+export { COLOUR_PALETTE, LINE_WEIGHT_PRESETS, OPACITY_PRESETS, RADIUS_PRESETS, DASH_PATTERN_PRESETS, SHAPE_PRESETS } from './presetPalette';
+export type { PresetValue } from './presetPalette';
+export { getPropertiesForKind, resolvePropertiesForKinds } from './stylePropertyMap';
+//# sourceMappingURL=index.d.ts.map
