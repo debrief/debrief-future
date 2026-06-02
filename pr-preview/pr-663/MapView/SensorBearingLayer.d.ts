996a18f49cb2e65cd39237d753a50ae427ac9987
IanMayo <IanMayo@users.noreply.github.com>
2026-06-02 12:29:19 +0000

    deploy: 11755b57c47b81705eef4301d16423a01206b304

---
+++ pr-preview/pr-663/MapView/SensorBearingLayer.d.ts
@@ -0,0 +1,4 @@
+import { SensorBearingLayerProps } from './sensor-utils';
+
+export declare function SensorBearingLayer({ feature, currentTime, displayMode, hiddenIds, }: SensorBearingLayerProps): null;
+//# sourceMappingURL=SensorBearingLayer.d.ts.map
